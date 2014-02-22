699282cfbbb3c3ab932fe2c363fc284558943da8
Shunta Saito <shunta.saito@gmail.com>
2014-02-22 00:46:56 -0800

    erase debug from some dependencies options

--- caffe.rb
+++ caffe.rb
@@ -5,14 +5,14 @@ class Caffe < Formula
   url "https://github.com/BVLC/caffe.git"
   version "1.0"
 
-  depends_on 'mitmul/caffe/cmake-std' => %w{build-from-source fresh verbose debug}
-  depends_on 'mitmul/caffe/boost-std' => %w{build-from-source fresh verbose debug}
-  depends_on 'mitmul/caffe/snappy-std' => %w{build-from-source fresh verbose debug}
-  depends_on 'mitmul/caffe/leveldb-std' => %w{build-from-source fresh verbose debug}
-  depends_on 'mitmul/caffe/protobuf-std' => %w{build-from-source fresh verbose debug}
-  depends_on 'mitmul/caffe/gflags-std' => %w{build-from-source fresh verbose debug}
-  depends_on 'mitmul/caffe/glog-std' => %w{build-from-source fresh verbose debug}
-  depends_on 'mitmul/caffe/opencv-std' => %w{build-from-source fresh verbose debug}
+  depends_on 'mitmul/caffe/cmake-std' => %w{build-from-source fresh}
+  depends_on 'mitmul/caffe/boost-std' => %w{build-from-source fresh debug}
+  depends_on 'mitmul/caffe/snappy-std' => %w{build-from-source fresh}
+  depends_on 'mitmul/caffe/leveldb-std' => %w{build-from-source fresh}
+  depends_on 'mitmul/caffe/protobuf-std' => %w{build-from-source fresh}
+  depends_on 'mitmul/caffe/gflags-std' => %w{build-from-source fresh}
+  depends_on 'mitmul/caffe/glog-std' => %w{build-from-source fresh}
+  depends_on 'mitmul/caffe/opencv-std' => %w{build-from-source fresh}
 
   def install
     system "sed -e '/^PYTHON_INCLUDES/ s/\\/usr\\/include/~\\/anaconda\\/include/g' -e '/numpy/ s/\\/usr\\/local/~\\/anaconda/g' -e '/CXX/ s/\\/usr\\/bin\\/g++/\\/usr\\/bin\\/clang++/g' -e '/CXXFLAGS/ s/#CXXFLAGS/CXXFLAGS/' Makefile.config.example Makefile.config"
@@ -21,6 +21,5 @@ class Caffe < Formula
 
     (lib + 'caffe').install Dir['libcaffe*']
     include.install Dir['include/*']
-
   end
 end
