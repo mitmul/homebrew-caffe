0f7d040a22bed5840931694be69ce4593c2f8d3d
Shunta Saito <shunta.saito@gmail.com>
2014-02-22 00:51:33 -0800

    add environment variables when installing

--- caffe.rb
+++ caffe.rb
@@ -15,6 +15,15 @@ class Caffe < Formula
   depends_on 'mitmul/caffe/opencv-std' => %w{build-from-source fresh}
 
   def install
+    ENV.append 'LD_LIBRARY_PATH', '/Developer/NVIDIA/CUDA-5.5/lib'
+    ENV.append 'LD_LIBRARY_PATH', '/opt/intel/composer_xe_2013_sp1.1.103/compiler/lib'
+    ENV.append 'LD_LIBRARY_PATH', '/opt/intel/composer_xe_2013_sp1.1.103/mkl/lib'
+    ENV.append 'LD_LIBRARY_PATH', '/Users/saito/anaconda/lib'
+    ENV.append 'LD_LIBRARY_PATH', '/usr/local/lib:/usr/lib:/lib'
+    ENV.append 'MKL_DIR', '/opt/intel/composer_xe_2013_sp1.1.103'
+    ENV.append 'DYLD_LIBRARY_PATH', '/opt/intel/composer_xe_2013_sp1.1.103/compiler/lib'
+    ENV.append 'DYLD_LIBRARY_PATH', '/opt/intel/composer_xe_2013_sp1.1.103/mkl/lib'
+
     system "sed -e '/^PYTHON_INCLUDES/ s/\\/usr\\/include/~\\/anaconda\\/include/g' -e '/numpy/ s/\\/usr\\/local/~\\/anaconda/g' -e '/CXX/ s/\\/usr\\/bin\\/g++/\\/usr\\/bin\\/clang++/g' -e '/CXXFLAGS/ s/#CXXFLAGS/CXXFLAGS/' Makefile.config.example Makefile.config"
     system "make"
     system "make pycaffe"
