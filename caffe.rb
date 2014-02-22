43872a0c42d7bc4c20fb3edeb63cfaf0ecd175bf
Shunta Saito <shunta.saito@gmail.com>
2014-02-21 22:38:03 -0800

    fix description for dependencies

--- caffe.rb
+++ caffe.rb
@@ -5,18 +5,17 @@ class Caffe < Formula
   url "https://github.com/BVLC/caffe.git"
   version "1.0"
 
+  depends_on 'mitmul/caffe/cmake' => %w{build-from-source fresh vd}
+  depends_on 'mitmul/caffe/boost' => %w{build-from-source fresh vd}
+  depends_on 'mitmul/caffe/snappy' => %w{build-from-source fresh vd}
+  depends_on 'mitmul/caffe/leveldb' => %w{build-from-source fresh vd}
+  depends_on 'mitmul/caffe/protobuf' => %w{build-from-source fresh vd}
+  depends_on 'mitmul/caffe/gflags' => %w{build-from-source fresh vd}
+  depends_on 'mitmul/caffe/glog' => %w{build-from-source fresh vd}
+  depends_on 'mitmul/caffe/opencv' => %w{build-from-source fresh vd}
+
   def install
-
-    system "brew", "install", "--build-from-source", "--fresh", "-vd", "mitmul/caffe/cmake"
-    system "brew", "install", "--build-from-source", "--fresh", "-vd", "mitmul/caffe/boost"
-    system "brew", "install", "--build-from-source", "--fresh", "-vd", "mitmul/caffe/snappy"
-    system "brew", "install", "--build-from-source", "--fresh", "-vd", "mitmul/caffe/leveldb"
-    system "brew", "install", "--build-from-source", "--fresh", "-vd", "mitmul/caffe/protobuf"
-    system "brew", "install", "--build-from-source", "--fresh", "-vd", "mitmul/caffe/gflags"
-    system "brew", "install", "--build-from-source", "--fresh", "-vd", "mitmul/caffe/glog"
-    system "brew", "install", "--build-from-source", "--fresh", "-vd", "mitmul/caffe/opencv"
     system "sed", "-e", "\"/^PYTHON_INCLUDES/ s/\/usr\/include/~\/anaconda\/include/g\"", "-e", "\"/numpy/ s/\/usr\/local/~\/anaconda/g", "-e", "\"/CXX/ s/\/usr\/bin\/g++/\/usr\/bin\/clang++/g\"", "-e", "\"/CXXFLAGS/ s/#CXXFLAGS/CXXFLAGS/\"", "Makefile.config.example", "Makefile.config"
-
     system "make"
     system "make pycaffe"
 
