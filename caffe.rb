6db5fc639312caf8a8002f54bfc7eef3214ee55e
Shunta Saito <shunta.saito@gmail.com>
2014-02-21 23:01:19 -0800

    fix dependencies names

--- caffe.rb
+++ caffe.rb
@@ -5,17 +5,17 @@ class Caffe < Formula
   url "https://github.com/BVLC/caffe.git"
   version "1.0"
 
-  depends_on 'mitmul/caffe/cmake' => %w{build-from-source fresh verbose}
-  depends_on 'mitmul/caffe/boost' => %w{build-from-source fresh verbose}
-  depends_on 'mitmul/caffe/snappy' => %w{build-from-source fresh verbose}
-  depends_on 'mitmul/caffe/leveldb' => %w{build-from-source fresh verbose}
-  depends_on 'mitmul/caffe/protobuf' => %w{build-from-source fresh verbose}
-  depends_on 'mitmul/caffe/gflags' => %w{build-from-source fresh verbose}
-  depends_on 'mitmul/caffe/glog' => %w{build-from-source fresh verbose}
-  depends_on 'mitmul/caffe/opencv' => %w{build-from-source fresh verbose}
+  depends_on 'mitmul/caffe/cmake-std' => %w{build-from-source fresh verbose}
+  depends_on 'mitmul/caffe/boost-std' => %w{build-from-source fresh verbose}
+  depends_on 'mitmul/caffe/snappy-std' => %w{build-from-source fresh verbose}
+  depends_on 'mitmul/caffe/leveldb-std' => %w{build-from-source fresh verbose}
+  depends_on 'mitmul/caffe/protobuf-std' => %w{build-from-source fresh verbose}
+  depends_on 'mitmul/caffe/gflags-std' => %w{build-from-source fresh verbose}
+  depends_on 'mitmul/caffe/glog-std' => %w{build-from-source fresh verbose}
+  depends_on 'mitmul/caffe/opencv-std' => %w{build-from-source fresh verbose}
 
   def install
-    system "sed", "-e", "\"/^PYTHON_INCLUDES/ s/\/usr\/include/~\/anaconda\/include/g\"", "-e", "\"/numpy/ s/\/usr\/local/~\/anaconda/g", "-e", "\"/CXX/ s/\/usr\/bin\/g++/\/usr\/bin\/clang++/g\"", "-e", "\"/CXXFLAGS/ s/#CXXFLAGS/CXXFLAGS/\"", "Makefile.config.example", "Makefile.config"
+    system "sed -e '/^PYTHON_INCLUDES/ s/\\/usr\\/include/~\\/anaconda\\/include/g' -e '/numpy/ s/\\/usr\\/local/~\\/anaconda/g' -e '/CXX/ s/\\/usr\\/bin\\/g++/\\/usr\\/bin\\/clang++/g' -e '/CXXFLAGS/ s/#CXXFLAGS/CXXFLAGS/' Makefile.config.example Makefile.config"
     system "make"
     system "make pycaffe"
 
